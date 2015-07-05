7dfe59e6ae3f08f2d5a87c509b2ce3a0f6698078
shihwei <shihwei.chuang@gmail.com>
2015-07-05 23:38:55 +0800

    add ContractController & Views

---
+++ Homework001/Controllers/ContractController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Homework001.Models;
+
+namespace Homework001.Controllers
+{
+    public class ContractController : Controller
+    {
+        private CustomerEntities db = new CustomerEntities();
+
+        // GET: Contract
+        public ActionResult Index()
+        {
+            var 客戶聯絡人 = db.客戶聯絡人.Include(客 => 客.客戶資料);
+            return View(客戶聯絡人.ToList());
+        }
+
+        // GET: Contract/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            客戶聯絡人 客戶聯絡人 = db.客戶聯絡人.Find(id);
+            if (客戶聯絡人 == null)
+            {
+                return HttpNotFound();
+            }
+            return View(客戶聯絡人);
+        }
+
+        // GET: Contract/Create
+        public ActionResult Create()
+        {
+            ViewBag.客戶Id = new SelectList(db.客戶資料, "Id", "客戶名稱");
+            return View();
+        }
+
+        // POST: Contract/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,客戶Id,職稱,姓名,Email,手機,電話")] 客戶聯絡人 客戶聯絡人)
+        {
+            if (ModelState.IsValid)
+            {
+                db.客戶聯絡人.Add(客戶聯絡人);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.客戶Id = new SelectList(db.客戶資料, "Id", "客戶名稱", 客戶聯絡人.客戶Id);
+            return View(客戶聯絡人);
+        }
+
+        // GET: Contract/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            客戶聯絡人 客戶聯絡人 = db.客戶聯絡人.Find(id);
+            if (客戶聯絡人 == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.客戶Id = new SelectList(db.客戶資料, "Id", "客戶名稱", 客戶聯絡人.客戶Id);
+            return View(客戶聯絡人);
+        }
+
+        // POST: Contract/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,客戶Id,職稱,姓名,Email,手機,電話")] 客戶聯絡人 客戶聯絡人)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(客戶聯絡人).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.客戶Id = new SelectList(db.客戶資料, "Id", "客戶名稱", 客戶聯絡人.客戶Id);
+            return View(客戶聯絡人);
+        }
+
+        // GET: Contract/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            客戶聯絡人 客戶聯絡人 = db.客戶聯絡人.Find(id);
+            if (客戶聯絡人 == null)
+            {
+                return HttpNotFound();
+            }
+            return View(客戶聯絡人);
+        }
+
+        // POST: Contract/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            客戶聯絡人 客戶聯絡人 = db.客戶聯絡人.Find(id);
+            db.客戶聯絡人.Remove(客戶聯絡人);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
