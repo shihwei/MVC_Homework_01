81a369ef57129422506bf8909ce42374e63608dd
shihwei <shihwei.chuang@gmail.com>
2015-07-06 09:46:33 +0800

    該專案要在資料庫中建立一個檢視表，並且匯入 EDMX 檔。

--- Homework001/Models/DataModel.Context.cs
+++ Homework001/Models/DataModel.Context.cs
@@ -28,5 +28,6 @@ namespace Homework001.Models
         public virtual DbSet<客戶資料> 客戶資料 { get; set; }
         public virtual DbSet<客戶銀行資訊> 客戶銀行資訊 { get; set; }
         public virtual DbSet<客戶聯絡人> 客戶聯絡人 { get; set; }
+        public virtual DbSet<vwCustomerSimpleView> vwCustomerSimpleView { get; set; }
     }
 }
