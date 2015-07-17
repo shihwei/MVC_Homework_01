ab55d89337ddb77866fa69e45606f97c19af3647
shihwei <shihwei.chuang@gmail.com>
2015-07-17 14:30:38 +0800

    加入 Partial Class & Metadatatype Code Gen

---
+++ Homework001/Models/vwCustomerSimpleView.Partial.cs
@@ -0,0 +1,23 @@
+namespace Homework001.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    
+    [MetadataType(typeof(vwCustomerSimpleViewMetaData))]
+    public partial class vwCustomerSimpleView
+    {
+    }
+    
+    public partial class vwCustomerSimpleViewMetaData
+    {
+        
+        [StringLength(50, ErrorMessage="欄位長度不得大於 50 個字元")]
+        [Required]
+        public string 客戶名稱 { get; set; }
+        [Required]
+        public int 聯絡人數量 { get; set; }
+        [Required]
+        public int 銀行帳戶數量 { get; set; }
+    }
+}
